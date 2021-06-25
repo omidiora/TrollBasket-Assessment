a802d9e43dc1875e449c802a4904c990faa139fc
omidiora <omidioraemmanuel@gmail.com>
2021-06-25 12:53:41 +0100

    add path

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,16 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/", productRoutes);
+// app.use("/", productRoutes);
+
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontent/build', 'index.html'));
+});
+
+
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend/build")));
@@ -18,8 +27,8 @@ if (process.env.NODE_ENV === "production") {
     response.sendFile(path.join(__dirname, "frontend/build", "index.html"));
   });
 }
-app.use("/", productRoutes);
-app.use("/api/products", productRoutes);
+// app.use("/", productRoutes);
+// app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
