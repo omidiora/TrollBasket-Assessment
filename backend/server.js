ace784547218bb5f487131ef02f7ad0df6872918
omidiora <omidioraemmanuel@gmail.com>
2021-06-25 12:14:53 +0100

    fx

--- backend/server.js
+++ backend/server.js
@@ -9,15 +9,16 @@ const app = express();
 
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res.json({ message: "API running..." });
-// });
+app.get("/", (req, res) => {
+  res.json({ message: "API running..." });
+});
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend/build")));
   app.get("*", (request, response) => {
     response.sendFile(path.join(__dirname, "frontend/build", "index.html"));
   });
 }
+app.use("/", productRoutes);
 app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
