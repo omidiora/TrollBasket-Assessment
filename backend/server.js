dd69e40f06f56e9e03c8d1ac73a37203b932763f
omidiora <omidioraemmanuel@gmail.com>
2021-06-25 12:57:25 +0100

    add path

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontent/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
 });
 
 
