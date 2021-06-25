3b267d0ebec36fbd46fc39f738cf8f24301ba87b
omidiora <omidioraemmanuel@gmail.com>
2021-06-25 12:58:19 +0100

    add path

--- backend/server.js
+++ backend/server.js
@@ -12,19 +12,19 @@ app.use(express.json());
 
 // app.use("/", productRoutes);
 
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+// app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
-});
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+// });
 
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/build")));
-  app.get("/", (request, response) => {
-    response.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+  app.use(express.static(path.join(__dirname, "build")));
+  app.get("*", (request, response) => {
+    response.sendFile(path.join(__dirname, "build", "index.html"));
   });
 }
 // app.use("/", productRoutes);
